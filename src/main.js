126a0ed18b36d55c0a11aa312f01cda85042da57
Nothingeid <Nothingeid@gmail.com>
2019-03-20 10:29:41 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ class LINE extends Command {
     }
 
     get myBot() {
-        const bot = ['ufe1707ae9b2ff7ab61505795b7995440','u6949f816dddef051b5762322e5597f1e'];
+        const bot = ['uecd0cafaba403830421fcda75309c860','u6949f816dddef051b5762322e5597f1e'];
         return bot; 
     }
 
